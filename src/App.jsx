91af8986540541223bd45ca67d08ab8696c90ede
Durgaprasadnaikkethavath <kethavathdurgaprasadnaik@gmail.com>
2024-10-22 17:59:57 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ContextApi from "./Context/ContextApi";
+
+const App = () => {
+  return (
+    <div>
+      <ContextApi />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/Context/ContextApi.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ContextApi = () => {
+  return (
+    <>
+      <h1>ContextApi</h1>
+    </>
+  );
+};
+
+export default ContextApi;
